371e8ca6ce88e2c7339fe0fa1c6ed7f3879a7eca
Henry BV Nguyen <hbn2018@icloud.com>
2017-06-11 21:00:53 -0700

    rails generate, rake, back-end routes, MVC set up

--- Gemfile
+++ Gemfile
@@ -24,3 +24,5 @@ source 'https://rubygems.org'
  gem 'jquery-rails'
  # Turbolinks makes following links in your web application faster. Read more: https://github.com/rails/turbolinks
  gem 'turbolinks'
+
+ gem 'thor', '0.19.1'
--- config/routes.rb
+++ config/routes.rb
@@ -1,4 +1,11 @@
 Rails.application.routes.draw do
+  get 'welcome/index'
+
+  get 'welcome/about'
+
+  root 'welcome#index' # root method takes hash as an argument
+  # root({to: 'welcome#index'})
+
   # The priority is based upon order of creation: first created -> highest priority.
   # See how all your routes lay out with "rake routes".
 
