c04b19c3daede264dda30e7e3c1308e42a1550a3
Henry BV Nguyen <hbn2018@icloud.com>
2017-06-11 21:06:09 -0700

    assignment - added contact page html and route

--- app/controllers/welcome_controller.rb
+++ app/controllers/welcome_controller.rb
@@ -4,4 +4,8 @@ class WelcomeController < ApplicationController
 
   def about
   end
+
+  def contact
+  end
+  
 end
--- config/routes.rb
+++ config/routes.rb
@@ -3,6 +3,8 @@ Rails.application.routes.draw do
 
   get 'welcome/about'
 
+  get 'welcome/contact'
+
   root 'welcome#index' # root method takes hash as an argument
   # root({to: 'welcome#index'})
 
