6b20b0aaea50905ebbaa4afdb84b374cb4add90a
Henry BV Nguyen <hbn2018@icloud.com>
2017-06-14 11:39:09 -0700

    all posts

--- app/controllers/posts_controller.rb
+++ app/controllers/posts_controller.rb
@@ -31,6 +31,34 @@ class PostsController < ApplicationController
   end
 
   def edit
+    @post = Post.find(params[:id])
   end
+
+  def update
+   @post = Post.find(params[:id])
+   @post.title = params[:post][:title]
+   @post.body = params[:post][:body]
+   
+   if @post.save
+     flash[:notice] = "Post was updated."
+     redirect_to @post
+   else
+     flash.now[:alert] = "There was an error saving the post. Please try again."
+     render :edit
+   end
+ end
+
+ def destroy
+   @post = Post.find(params[:id])
+   
+ # #8
+ if @post.destroy
+   flash[:notice] = "\"#{@post.title}\" was deleted successfully."
+   redirect_to posts_path
+ else
+   flash.now[:alert] = "There was an error deleting the post."
+   render :show
+ end
+end
 
 end
--- spec/controllers/posts_controller_spec.rb
+++ spec/controllers/posts_controller_spec.rb
@@ -57,23 +57,86 @@ end
 end
 
 
- describe "GET show" do
-   it "returns http success" do
+describe "GET show" do
+ it "returns http success" do
 # #16
-     get :show, {id: my_post.id}
-     expect(response).to have_http_status(:success)
-   end
-   it "renders the #show view" do
+get :show, {id: my_post.id}
+expect(response).to have_http_status(:success)
+end
+it "renders the #show view" do
 # #17
-     get :show, {id: my_post.id}
-     expect(response).to render_template :show
-   end
+get :show, {id: my_post.id}
+expect(response).to render_template :show
+end
 
-   it "assigns my_post to @post" do
-     get :show, {id: my_post.id}
+it "assigns my_post to @post" do
+ get :show, {id: my_post.id}
 # #18
-     expect(assigns(:post)).to eq(my_post)
-   end
+expect(assigns(:post)).to eq(my_post)
+end
+end
+
+describe "GET edit" do
+ it "returns http success" do
+   get :edit, {id: my_post.id}
+   expect(response).to have_http_status(:success)
  end
+ 
+ it "renders the #edit view" do
+   get :edit, {id: my_post.id}
+ # #1
+ expect(response).to render_template :edit
+end
+
+ # #2
+ it "assigns post to be updated to @post" do
+   get :edit, {id: my_post.id}
+   
+   post_instance = assigns(:post)
+   
+   expect(post_instance.id).to eq my_post.id
+   expect(post_instance.title).to eq my_post.title
+   expect(post_instance.body).to eq my_post.body
+ end
+end
+
+describe "PUT update" do
+ it "updates post with expected attributes" do
+   new_title = RandomData.random_sentence
+   new_body = RandomData.random_paragraph
+   
+   put :update, id: my_post.id, post: {title: new_title, body: new_body}
+   
+ # #3
+ updated_post = assigns(:post)
+ expect(updated_post.id).to eq my_post.id
+ expect(updated_post.title).to eq new_title
+ expect(updated_post.body).to eq new_body
+end
+
+it "redirects to the updated post" do
+ new_title = RandomData.random_sentence
+ new_body = RandomData.random_paragraph
+ 
+ # #4
+ put :update, id: my_post.id, post: {title: new_title, body: new_body}
+ expect(response).to redirect_to my_post
+end
+end
+
+describe "DELETE destroy" do
+ it "deletes the post" do
+   delete :destroy, {id: my_post.id}
+ # #6
+ count = Post.where({id: my_post.id}).size
+ expect(count).to eq 0
+end
+
+it "redirects to posts index" do
+ delete :destroy, {id: my_post.id}
+ # #7
+ expect(response).to redirect_to posts_path
+end
+end
 
 end
