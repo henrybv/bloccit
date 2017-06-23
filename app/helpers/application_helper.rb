9fa0acb2b5d1a22738473ad76197408796bb67aa
Henry BV Nguyen <hbn2018@icloud.com>
2017-06-22 17:08:28 -0700

    module3-checkpoint23 complete

--- app/helpers/application_helper.rb
+++ app/helpers/application_helper.rb
@@ -1,2 +1,8 @@
 module ApplicationHelper
+	def form_group_tag(errors, &block)
+		css_class = 'form-group'
+		css_class << ' has-error' if errors.any?
+# #5
+		content_tag :div, capture(&block), class: css_class
+	end
 end
--- spec/models/post_spec.rb
+++ spec/models/post_spec.rb
@@ -11,6 +11,12 @@ RSpec.describe Post, type: :model do
    let(:post) { topic.posts.create!(title: title, body: body) }
  
    it { is_expected.to belong_to(:topic) }
+   it { is_expected.to validate_presence_of(:title) }
+   it { is_expected.to validate_presence_of(:body) }
+   it { is_expected.to validate_presence_of(:topic) }
+ 
+   it { is_expected.to validate_length_of(:title).is_at_least(5) }
+   it { is_expected.to validate_length_of(:body).is_at_least(20) }
  
  # #2
    describe "attributes" do
